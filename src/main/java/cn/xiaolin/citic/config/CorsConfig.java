8319a018421dda2e63f9374f6d108cb63b554dc4
xingxiaolin <xlxing@bupt.edu.cn>
2023-11-17 10:33:42 +0800

    [feat]allowed cors

---
+++ src/main/java/cn/xiaolin/citic/config/CorsConfig.java
@@ -0,0 +1,38 @@
+package cn.xiaolin.citic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author xingxiaolin xlxing@bupt.edu.cn
+ * @Description
+ * @create 2023/11/17
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        //1. 添加 CORS配置信息
+        CorsConfiguration config = new CorsConfiguration();
+        //允许所有Origin的所有方法跨域请求
+        config.addAllowedOriginPattern("*");
+        //是否发送 Cookie
+        config.setAllowCredentials(true);
+        //放行哪些请求方式
+        config.addAllowedMethod("*");
+        //放行哪些原始请求头部信息
+        config.addAllowedHeader("*");
+        //暴露哪些头部信息
+        config.addExposedHeader("*");
+        //2. 添加映射路径
+        UrlBasedCorsConfigurationSource corsConfigurationSource
+                = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**", config);
+        //3. 返回新的CorsFilter
+        return new CorsFilter(corsConfigurationSource);
+    }
+}
